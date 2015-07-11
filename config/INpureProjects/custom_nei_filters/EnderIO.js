eb9359b27f5dc0b4349d83dd755f3a0fe620d211
Saereth <guiken@gmail.com>
2015-07-10 20:40:28 -0400

    Inpure Core work

--- config/INpureProjects/custom_nei_filters/EnderIO.js
+++ config/INpureProjects/custom_nei_filters/EnderIO.js
@@ -1,11 +1,10 @@
 if (FML.isModLoaded("EnderIO") && EnderIO_enabled) {
     NEI.hide("EnderIO:itemGliderWing");
-    NEI.hide("EnderIO:itemGliderWing");
 	NEI.hide("EnderIO:item.darkSteel_chestplate");
     NEI.hide("EnderIO:item.darkSteel_helmet");
     NEI.hide("EnderIO:item.darkSteel_leggings");
-	NEI.hide("EnderIO:item.darkSteel_boots");
-    NEI.hide("EnderIO:item.darkSteel_sword");
+	NEI.hide("EnderIO:item.darkSteel_boots"); 
+	NEI.hide("EnderIO:item.darkSteel_sword");
     NEI.hide("EnderIO:item.darkSteel_pickaxe");
 	NEI.hide("EnderIO:item.darkSteel_axe");
     NEI.hide("EnderIO:item.darkSteel_shears");
@@ -13,7 +12,7 @@ if (FML.isModLoaded("EnderIO") && EnderIO_enabled) {
 	NEI.hide("EnderIO:blockAlloySmelter");
     NEI.hide("EnderIO:blockSagMill");
 	NEI.hide("EnderIO:blockSolarPanel");
-    NEI.hide("EnderIO:blockZombieGenerator");
+	NEI.hide("EnderIO:blockZombieGenerator");
     NEI.hide("EnderIO:blockCombustionGenerator");
 	NEI.hide("EnderIO:blockStirlingGenerator");
     NEI.hide("EnderIO:itemMagnet");
@@ -29,5 +28,5 @@ if (FML.isModLoaded("EnderIO") && EnderIO_enabled) {
     NEI.hide("EnderIO:blockCrafter");
 	NEI.hide("EnderIO:blockTransceiver");
 	NEI.hide("EnderIO:blockPowerMonitor");
-	NEI.hide("EnderIO:blockCapBank);
+	NEI.hide("EnderIO:blockCapBank");
 }
